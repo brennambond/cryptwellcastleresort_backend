3b9edf8c263815cdbf8f449bb3f96a5ee43ce451
Brennam Bond <brennambond@gmail.com>
2024-12-11 21:41:54 -0600

    Updates to backend

--- useraccount/api.py
+++ useraccount/api.py
@@ -9,6 +9,17 @@ from room.serializers import ReservationsListSerializer
 
 
 @api_view(['GET'])
+@authentication_classes([])
+@permission_classes([])
+def user_detail(request, pk):
+    user = User.objects.get(pk=pk)
+
+    serializer = UserDetailSerializer(user, many=False)
+
+    return JsonResponse(serializer.data, safe=False)
+
+
+@api_view(['GET'])
 def reservations_list(request):
     reservations = request.user.reservations.all()
     serializer = ReservationsListSerializer(reservations, many=True)
--- useraccount/models.py
+++ useraccount/models.py
@@ -8,7 +8,7 @@ from django.db import models
 class CustomUserManager(UserManager):
     def _create_user(self, name, email, password, **extra_fields):
         if not email:
-            raise ValueError("You have not specified a valid email-address")
+            raise ValueError("You have not specified a valid e-mail address")
 
         email = self.normalize_email(email)
         user = self.model(email=email, name=name, **extra_fields)
