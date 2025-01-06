d63cc42dc71f815c7ebe02a80421d8480a2cbb48
Brennam Bond <brennambond@gmail.com>
2025-01-06 12:02:29 -0600

    Updatest to settings.py for image_url issues

--- hh_api/settings.py
+++ hh_api/settings.py
@@ -246,7 +246,8 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_AKI')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SAK')
 
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_SBN')
+AWS_STORAGE_BUCKET_NAME = os.environ.get(
+    'AWS_SBN', "hauntedhotel-backend-bucket")
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_FILE_OVERWRITE = True
 
