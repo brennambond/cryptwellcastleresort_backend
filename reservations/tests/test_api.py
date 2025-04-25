39096978fc73b9e7aff92faae3ad0138d20f91ce
Brennam Bond <brennambond@gmail.com>
2025-04-25 15:17:48 -0500

    Create user for test in test_api.py file to ensure passing test

--- reservations/tests/test_api.py
+++ reservations/tests/test_api.py
@@ -1,10 +1,18 @@
 import pytest
-from django.urls import reverse
 from rest_framework.test import APIClient
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+User = get_user_model()
 
 
 @pytest.mark.django_db
 def test_reservation_list_authenticated_user():
+    user = User.objects.create_user(username='testuser', password='testpass')
+
     client = APIClient()
+    client.force_authenticate(user=user)
+
     response = client.get(reverse("list_user_reservations"))
+
     assert response.status_code == 200
