7fb5662bef4abb30065b1b0eb69fb23f2d205dd9
Brennam Bond <brennambond@gmail.com>
2025-07-02 16:44:42 -0500

    Update to admin.py for Rooms

--- room/admin.py
+++ room/admin.py
@@ -4,7 +4,7 @@ from .models import Wing, Category, Room
 
 @admin.register(Wing)
 class WingAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'description', 'image_url')
+    list_display = ('id', 'name', 'description', 'image')
     search_fields = ('name',)
 
 
@@ -17,7 +17,7 @@ class CategoryAdmin(admin.ModelAdmin):
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'price_per_night',
-                    'wing', 'category', 'beds', 'bedrooms', 'bathrooms', 'guests', 'created_at', 'image_url')
+                    'wing', 'category', 'beds', 'bedrooms', 'bathrooms', 'guests', 'created_at', 'image')
     list_filter = ('wing', 'category')
     search_fields = ('title', 'description', 'price_per_night', 'wing',
                      'category', 'beds', 'bedrooms', 'bathrooms', 'guests')
